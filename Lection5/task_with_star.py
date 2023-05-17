c917dcd4ef145ec7a4c5841bf45383a7bc055510
ALEAS90 <a.n.asafov@yandex.ru>
2023-05-17 16:12:03 +0300

    решаю  задание под звездочкой 5 лекции

---
+++ Lection5/task_with_star.py
@@ -0,0 +1,40 @@
+# Напишите функцию to_roman, которая преобразуют арабское число (val) в римское (roman_str).
+#
+# Современные римские цифры записываются, выражая каждую цифру отдельно,
+# начиная с самой левой цифры и пропуская цифру со значением нуля.
+# Римскими цифрами 1990 отображается: 1000=М, 900=СМ, 90=ХС; в результате MCMXC.
+# 2023 записывается как 2000=MM, 20=XX, 3=III; или MMXXIII.
+# В 1666 используется каждый римский символ в порядке убывания: MDCLXVI.
+#
+# Например (Ввод --> Вывод) :
+# 2008 --> MMVIII
+
+
+def to_roman(data):
+    ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    hunds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    thous = ["", "M", "MM", "MMM", "MMMM"]
+
+    t = thous[data // 1000]
+    h = hunds[data // 100 % 10]
+    te = tens[data // 10 % 10]
+    o = ones[data % 10]
+
+    return t + h + te + o
+    # return result
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [1133, 2224, 1938, 1817, 2505, 391, 3743, 1634, 699, 1666, 1494, 1444]
+
+test_data = [
+    "MCXXXIII", "MMCCXXIV", "MCMXXXVIII", "MDCCCXVII", "MMDV", "CCCXCI", 'MMMDCCXLIII', 'MDCXXXIV', 'DCXCIX', 'MDCLXVI',
+    'MCDXCIV', 'MCDXLIV']
+
+
+for i, d in enumerate(data):
+    assert to_roman(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
