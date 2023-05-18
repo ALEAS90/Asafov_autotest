79f318dd3e2330aa35df3d09c03ebd70ceeda70a
ALEAS90 <a.n.asafov@yandex.ru>
2023-05-18 11:47:00 +0300

    111

--- key2.py
+++ key2.py
@@ -1,21 +1,23 @@
 
-def update_dictionary(d, key, value):   #(d, key, value):
+def update_dictionary(d, key, value):   #(d, key, value): {2: [3,4]}
     d = {}
+    key = 2
+    value = 3
     # key = input()
     # value = input()
     if key in d.keys():
-        d.update(key,value)
+        d.add(key,value)
         # print(d)
     elif key is not d.keys() and key*2 in d.keys():
         d.setdefault(key*2, value)
     elif key is not d.keys() and key*2 is not d.keys():
         d.setdefault(key*2, value)
     print(d)
-    return update_dictionary(2,3,5)
+    return update_dictionary()
 
         # d.setdefault(2*key, value)
         # print(d)
-    # put your python code here
-    d = {2:[2]}
-    key =2
-    value = 4
+    # # put your python code here
+    # d = {2:[2]}
+    # key =2
+    # value = 4
