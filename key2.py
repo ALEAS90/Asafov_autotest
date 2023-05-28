daa89fb76df05edece621d0b1682c6a0f835a9ae
ALEAS90 <a.n.asafov@yandex.ru>
2023-05-28 20:58:24 +0300

    пример из лекции

--- key2.py
+++ key2.py
@@ -1,24 +1,14 @@
 
-def update_dictionary(d, key, value):   #(d, key, value): {2: [3,4]}
-    d = {2: 3}
-    key = 2
-    value = 4
+def update_dictionary(d, key, value):
+    d = {}
     # key = input()
     # value = input()
     if key in d.keys():
-        d.setdefault(key, value).append(value)
-        print(d)
+        d.update(key,value)
+        # print(d)
     elif key is not d.keys() and key*2 in d.keys():
         d.setdefault(key*2, value)
     elif key is not d.keys() and key*2 is not d.keys():
         d.setdefault(key*2, value)
     print(d)
-    return update_dictionary()
-update_dictionary()
-
-        # d.setdefault(2*key, value)
-        # print(d)
-    # # put your python code here
-    # d = {2:[2]}
-    # key =2
-    # value = 4
+update_dictionary({2:3}, 2, 1)
