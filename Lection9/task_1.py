c91d097e14c1df83270d312cb10b2453501e6bae
ALEAS90 <a.n.asafov@yandex.ru>
2023-06-01 22:44:12 +0300

    решаю 1 задание 9 лекции

--- Lection9/task_1.py
+++ Lection9/task_1.py
@@ -5,42 +5,30 @@
 from pathlib import Path
 
 path = Path('C:/development/pythonProject/Lection9/test_file/task1_data.txt')
-print((path.is_file()))
+# print((path.is_file()))
 file = open(path, mode='r', encoding='utf-8')
 # print(file.read())
 line = ''
+text_new = ''
+def replace_text(file):
+    for line in file.readlines():
+        for symbol in line:
+            if symbol.isdigit() == False:
+                text_new += symbol
 
-for line in file.readlines():
-    for symbol in line:
-        if symbol.isdigit() == False:
-            line += symbol
+        print(text_new)
+        return text_new
 
+def rewrite_text_new_file(text_new)
+    path_new = Path
 
-
-
-    # print(line)
-
-
-
-
-
-# print(path)
-# print(Path.readlines(path))
-
-
-#
-#
-# def delete_nums(path):
-#     print(path.readlines())
-#     return delete_nums()
-# delete_nums(path)
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
 
 
-# with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
-#     with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
-#         answer = file1.readlines()
-#         ethalon = file2.readlines()
-#         assert answer == ethalon, "Файл ответа не совпадает с эталонном"
-# print('Всё ок')
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
