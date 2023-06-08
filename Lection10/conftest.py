260b5d69171662d3dc284262b593dd53b56d7a56
ALEAS90 <a.n.asafov@yandex.ru>
2023-06-08 16:41:53 +0300

    решаю задания 10 лекции

--- Lection10/conftest.py
+++ Lection10/conftest.py
@@ -4,8 +4,6 @@ import datetime
 @pytest.fixture() # Таким образом функция будет восприниматься как фикстура
 def time_start():
     time_start_1 = datetime.datetime.now()  # текущее время
-    # date_correct = time_start_1.strftime('%d.%m %H:%M:%S')  # текущее время по маске
-    # print(date_correct)
     yield time_start_1
     print('Время выполнения: ', datetime.datetime.now() - time_start_1)
 
@@ -13,8 +11,6 @@ def time_start():
 @pytest.fixture(scope='class')
 def for_class_fixt():
     time_start_1 = datetime.datetime.now()  # текущее время
-    # date_correct = time_start_1.strftime('%d.%m %H:%M:%S')  # текущее время по маске
-    # print(date_correct)
     yield time_start_1
 
     print('\n', 'Время выполнения класса: ', datetime.datetime.now() - time_start_1)
--- Lection10/task_3.py
+++ Lection10/task_3.py
@@ -10,40 +10,50 @@ def all_division(*arg1):
         division /= i
     return division
 
-all_division(2, 4, 89)
+# all_division(2, 4, 89)
+
+@pytest.mark.parametrize("a, b, result", [pytest.param(10, 10, 1, marks=pytest.mark.smoke),
+                                          (10, 1.6, 6.25),
+                                          (-7, 1, -7),
+                                          (0, 4, 0),
+                                          pytest.param(1, -2, -0.5, marks=pytest.mark.skip("тест скипнут"))])
+def test_value(a, b, result):
+    assert all_division(a, b) == result, 'проверка результата решения'
 
 
-"""Тесты"""
-
-@pytest.mark.smoke
-def test_value():
-    assert all_division(10, 10) == 1, 'проверка результата решения'
 
 
-# def test_zero():
-#     with pytest.raises(ZeroDivisionError):
-#         all_division(4, 3, 0, 8)
-
-@pytest.mark.parametrize('x',[4,1])
-@pytest.mark.parametrize('y',[0,0])
-def test_zero(x,y):
-    try:
-        all_division(x,y)
-    except ZeroDivisionError:
-        print('деление на ноль')
-    # with pytest.raises(ZeroDivisionError):
-    #      all_division(x, y)
-
-@pytest.mark.skip('скипнут исходя из условий задания')
-def test_numbers():
-    with pytest.raises(TypeError):
-        all_division(4, 'd', 0, 8)
 
 
-def test_index_error():
-    with pytest.raises(IndexError):
-        all_division(), 'не хватает аргументов'
 
-def test_negative_numbers():
-    assert all_division(1,-2,-2) == 0.25
-
+# @pytest.mark.smoke
+# def test_value():
+#     assert all_division(10, 10) == 1, 'проверка результата решения'
+#
+#
+# # def test_zero():
+# #     with pytest.raises(ZeroDivisionError):
+# #         all_division(4, 3, 0, 8)
+#
+# @pytest.mark.parametrize('x',[4,1])
+# @pytest.mark.parametrize('y',[0,0])
+# def test_zero(x,y):
+#     try:
+#         all_division(x,y)
+#     except ZeroDivisionError:
+#         print('деление на ноль')
+#     # with pytest.raises(ZeroDivisionError):
+#     #      all_division(x, y)
+#
+# @pytest.mark.skip('скипнут исходя из условий задания')
+# def test_numbers():
+#     with pytest.raises(TypeError):
+#         all_division(4, 'd', 0, 8)
+#
+#
+# def test_index_error():
+#     with pytest.raises(IndexError):
+#         all_division(), 'не хватает аргументов'
+#
+# def test_negative_numbers():
+#     assert all_division(1,-2,-2) == 0.25
